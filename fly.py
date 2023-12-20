43d96ad3a77ab6eefa3a67676c872265f8d959b7
Anastasija Midlbruka <amidlbruka@edu.riga.lv>
2023-12-20 11:52:08 +0200

    best code

--- fly.py
+++ fly.py
@@ -1,4 +1,6 @@
+import time
 from djitellopy import Tello
+
 
 # Uzdevums: Iziet trasi ar koda palidzību
 # Noteikumi:
@@ -22,7 +24,7 @@ tello.rotate_clockwise(180)
 
 tello.move_forward(123)
 tello.land()
-tello.time.sleep(3)
+time.sleep(3)
 tello.takeoff()
 tello.move_down(20)
 tello.rotate_counter_clockwise(90)
@@ -30,7 +32,7 @@ tello.move_forward(95)
 tello.rotate_clockwise(90)
 tello.move_forward(63)
 tello.land()
-tello.time.sleep(3)
+time.sleep(3)
 tello.takeoff()
 tello.move_down(20)
 tello.rotate_clockwise(90)
